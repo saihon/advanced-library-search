00b6a3b75b0c179e77c35c2a4fa943f7a042ee4b
saihon <devqrx+saihon-github@gmail.com>
2022-03-14 01:02:39 +0900

    Change index of open tab

--- src/ts/background.ts
+++ src/ts/background.ts
@@ -3,7 +3,11 @@ const openTab = () => {
     chrome.tabs.query({active : true, currentWindow : true}, tabs => {
         const tab = tabs[0];
         chrome.tabs.create(
-            {url : 'index.html', active : true, index : tab.index + 1});
+            {
+              url : 'index.html',
+              active : true,
+              // index : tab.index + 1,
+            });
     });
 };
 chrome.browserAction.onClicked.addListener(openTab);
